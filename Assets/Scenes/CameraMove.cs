ed3019c71c37cc6e1729877e7df12a2a1e154c51
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-12-25 21:36:49 +0300

    /

---
+++ Assets/Scenes/CameraMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float cameraSpeed = 5f;
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        
+      
+    }
+}
--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -10,46 +10,14 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private Camera _camera;
-    [SerializeField] private float speed;
-    [SerializeField] private float rotationSpeed;
+ 
+
+    
     [SerializeField] private TMP_Text _text;
     [SerializeField] private int pickObj;
     [SerializeField] private int max;
-    public float movementSpeed = 5f;
-    //public float rotationSpeed = 100f;
-    public float jumpForce = 5f;
+ 
     
-   // private Rigidbody rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Перемещение
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * movementSpeed * Time.deltaTime;
-        rb.MovePosition(transform.position + movement);
-
-        // Поворот
-        if (moveHorizontal != 0 || moveVertical != 0)
-        {
-            Vector3 newDirection = new Vector3(moveHorizontal, 0f, moveVertical);
-            Quaternion toRotation = Quaternion.LookRotation(newDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-
-        // Прыжок
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
-        {
-            rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
-        }
     }
     private void OnCollisionEnter(Collision other)
     {
