d104724615862f108bd737cba2fbc07d2bd0006d
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-12-25 16:32:55 +0300

    .

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -17,12 +17,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private TMP_Text _text;
     [SerializeField] private int pickObj;
     [SerializeField] private int max;
-    
-    private Vector3 movement;
-    private Vector3 movementDirection;
     public float movementSpeed = 5f;
+    //public float rotationSpeed = 100f;
     public float jumpForce = 5f;
     
+   // private Rigidbody rb;
 
     private void Start()
     {
@@ -52,8 +51,6 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
         }
     }
-
-
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("TriggeLva"))
